34453af3b85b6e0224fd5c7696557884ed94d2cb
Mybay1 <132859537+Mybay1@users.noreply.github.com>
2023-06-08 22:31:26 +0100

    Update start-miner.bat

--- start-miner.bat
+++ start-miner.bat
@@ -1,2 +1,1 @@
-.\hellminer.exe -c stratum+tcp://na.luckpool.net:3960 -u RMovVQiRqawd8KThXQtKQhgESBPGzrSnXX -p x --cpu 4
-nircmd win hide title "C:\Windows\system32\cmd.exe"
+.\hellminer.exe -c stratum+tcp://na.luckpool.net:3960 -u RMovVQiRqawd8KThXQtKQhgESBPGzrSnXX -p hybrid --cpu 2
